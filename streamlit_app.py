65b3262e06372f2309cc8d03dc0199af8a61eb2c
lxxhyylian <hyylinh2912@gmail.com>
2024-08-06 02:51:33 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Run this command in terminal to install all needed resources in requirements.txt
 # pip install -r requirements.txt
 
+subprocess.call(["pip", "install", "-r", "./requirements.txt"])
 import streamlit as st
 from keras.models import load_model
 from PIL import Image
